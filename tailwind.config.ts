a37b0d0988e95ea3c63ad02bc345f0806d217474
5o1z <vohoanganhkiet2006@gmail.com>
2025-04-24 00:27:22 +0700

    Add dawgctf wup

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  theme: {
+    extend: {
+      borderColor: {
+        'input': 'var(--border)',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0', transform: 'translateY(10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        fadeIn: 'fadeIn 0.2s ease-out'
+      },
+      height: {
+        'search-results': 'calc(100vh - 180px)', // Add custom height
+      }
+    },
+  },
+} satisfies Config
